98b24dba10d9a68ae2bf39710d895690ae93e769
Rishi Jha <rishijha2312@gmail.com>
2025-10-15 19:55:18 +0530

    added image carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,9 +4,11 @@ import { slider } from "../utils";
 const Carousel = () => {
     const [current, setCurrent] = useState(0);
     return (
-   <div>
+   <div className="relative">
        
             <img src={slider[current].image} />
+            <button className="absolute  top-[100px] bg-white p-5" onClick={() => setCurrent(current == 0 ? slider.length - 1 : current - 1)}> Prev </button>
+            <button className="absolute right-[5px] top-[100px] bg-white p-5" onClick={() => setCurrent(current == slider.length - 1 ? 0 : current + 1)}>Next</button>
        
    </div>
     )
